7e2c30b6302e2adadc1888c9e61d0c8ea35f1dc0
松杰 李 <lisongjie90@gmail.com>
2017-11-28 17:36:11 +0800

    Initial commit

---
+++ CombinSet.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+class CombinSets():
+    def __init__(self,set_list = []):
+        self.set_list = set_list
+    def CombinSets(self):
+        ds = []
+        for iset in self.set_list:
+            tds = []
+            tset = iset.copy()
+            for jset in ds:
+                if len(iset&jset)>0:
+                    tset |= jset
+                else:
+                    tds.append(jset)
+            tds.append(tset)
+            ds = tds
+        return ds
