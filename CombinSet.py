9927ea25223a8a8fe54ea7c8287e64b9063fcf6f
松杰 李 <lisongjie90@gmail.com>
2017-11-29 09:10:03 +0800

    Initial commit

--- CombinSet.py
+++ CombinSet.py
@@ -2,8 +2,17 @@
 
 class CombinSets():
     def __init__(self,set_list = []):
+        '''
+
+        :param set_list:
+        '''
         self.set_list = set_list
+
     def CombinSets(self):
+        '''
+
+        :return:
+        '''
         ds = []
         for iset in self.set_list:
             tds = []
